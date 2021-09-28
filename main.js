4288f90b35d79a75c074012915255fa4d027949f
FARADAY001 <46404071+FARADAY001@users.noreply.github.com>
2021-09-28 19:08:19 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 /* déclaration des constatntes pour les actions d'afficharge de mot de passe */
+
+
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
 
@@ -27,12 +29,82 @@ togglePasswordConfirm.addEventListener('click', function (e) {
 });
 
 
+/*  Condition de validation  
 
-   
+$(document).ready(function(){
+    $(function() {
+    // Setup form validation on the #register-form element
+   $("form[name='inscription-form']").validate({   //#register-form is form id
+       // Specify the validation rules
+  rules: {
+           nomComplet: "required", //firstname is corresponding input name   
+           email: {               //email is corresponding input name
+            required: true,
+            email: true
+        },
+
+        password: { //passowrd:  is corresponding input name
+          required: true,
+          minlength: 6
+        },
+           
+       },
+       // Specify the validation error messages
+  messages: {
+           nomComplet: "Enter First Name",
+           email: "Enter Passowrd",
+           password: "Enter Valid Email ID"
+       },
+       submitHandler: function(form) {
+           form.submit();
+       }
+   });
+  
+    });
+  })
+  */
 
 
+ /* 
+  $(function() {
+  
+    $("#myForm").validator();
+  
+    // when the form is submitted
+    $("#myForm").on("submit", function(e) {
+      // if the validator does not prevent form submit
+      if (!e.isDefaultPrevented()) {
+        var url = "inscription.php";
+  
+        var messageAlert = "alert-success";
+        var messageText =
+          "Your message was sent, thank you. I will get back to you soon.";
+  
+        // let's compose Bootstrap alert box HTML
+        var alertBox =
+          '<div class="alert ' +
+          messageAlert +
+          ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+          messageText +
+          "</div>";
+  
+        // If we have messageAlert and messageText
+        if (messageAlert && messageText) {
+          // inject the alert to .messages div in our form
+          $("#contact-form").find(".messages").html(alertBox);
+          // empty the form
+          $("#contact-form")[0].reset();
+        }
+  
+        return false;
+      }
+    });
+  });
 
+  */
 
+  /*********   test    
+var formValid = document.getElementById('bouton_envoi');
+var prenom = document.getElementById('nomComplet');
 
-
-
+*/
